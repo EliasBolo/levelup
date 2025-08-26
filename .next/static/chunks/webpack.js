6e46335f0f17d1db273584ed58df0df9d4163f9a
Elias Bolo <iliasbolo@gmail.com>
2025-08-27 01:08:25 +0300

    menu

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "10eab19df9863f2f"; }
+/******/ 		__webpack_require__.h = function() { return "573d4df077186d8d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.c05c111bc8ed06de.hot-update.js
+++ .next/static/webpack/webpack.c05c111bc8ed06de.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7ff6540ffac2a84c"; }
+/******/ 	__webpack_require__.h = function() { return "ca43e9a1b195b9f5"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
